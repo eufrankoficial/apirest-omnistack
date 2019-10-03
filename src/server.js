f3b6b3f4a063eea8a816650c152398a671f33048
Rafael Franke <rafinhamr.13@gmail.com>
2019-10-03 16:40:30 -0400

    Ajustes nos uploads

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express'); // ajudará na definições de rotas
 const mongoose = require('mongoose'); // ajudará na definições de banco de dados.
+const cors = require('cors'); // ajudará na definições de banco de dados.
+const path = require('path');
 
 const routes = require('./routes'); // importa rotas
 const app = express(); // roda a aplicação
@@ -10,7 +12,9 @@ mongoose.connect('mongodb+srv://frankedeveloper:frankedeveloper@omnistack-artek.
     useNewUrlParser: true
 });
 
+app.use(cors());
 app.use(express.json()); // fala pro express ler json
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes); // utiliza as rotas
 
 app.listen(3333); // escuta aplicação em localhost:3333
